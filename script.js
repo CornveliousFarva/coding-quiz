140ce8477e85f34945bfca521b73343f3e6f4872
Greg Schuman <gcschuman@hotmail.com>
2020-01-25 13:48:57 -0800

    Changed a couple of questions

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ var highScore = total + point;
         sessionStorage.setItem("a1", "B");
         sessionStorage.setItem("a2", "B");
         sessionStorage.setItem("a3", "C");
-        sessionStorage.setItem("a4", "B");
+        sessionStorage.setItem("a4", "A");
         sessionStorage.setItem("a5", "A");
         sessionStorage.setItem("a6", "D");
         sessionStorage.setItem("a7", "C");
@@ -31,73 +31,75 @@ $(document).ready(function(){
     //submit the first answer to the first question and each subsequent question
     $("#q1 #Next").click(function(){
         $(".questionForm").hide();
-        process("q1");
-        $("#q2").fadeIn(2000);
+        //process("q1");
+        $("#q2").fadeIn(200);
         return false;
     });
 
     $("#q2 #Next").click(function(){
         $(".questionForm").hide();
-        process("q2")
-        $("#q3").fadeIn(2000);
+        //process("q2")
+        $("#q3").fadeIn(200);
         return false;
     });
 
     $("#q3 #Next").click(function(){
         $(".questionForm").hide();
-        process("q3");
-        $("#q4").fadeIn(2000);
+        //process("q3");
+        $("#q4").fadeIn(200);
         return false;
     });
 
     $("#q4 #Next").click(function(){
         $(".questionForm").hide();
-        process("q4");
-        $("#q5").fadeIn(2000);
+        //process("q4");
+        $("#q5").fadeIn(200);
         return false;
     });
 
     $("#q5 #Next").click(function(){
         $(".questionForm").hide();
-        process("q5");
-        $("#q6").fadeIn(2000);
+        //process("q5");
+        $("#q6").fadeIn(200);
         return false;
     });
 
     $("#q6 #Next").click(function(){
         $(".questionForm").hide();
-        process("q6");
-        $("#q7").fadeIn(2000);
+        //process("q6");
+        $("#q7").fadeIn(200);
         return false;
     });
 
     $("#q7 #Next").click(function(){
         $(".questionForm").hide();
-        process("q7");
-        $("#q8").fadeIn(2000);
+        //process("q7");
+        $("#q8").fadeIn(200);
         return false;
     });
 
     $("#q8 #Next").click(function(){
         $(".questionForm").hide();
-        process("q8");
-        $("#q9").fadeIn(2000);
+        //process("q8");
+        $("#q9").fadeIn(200);
         return false;
     });
 
     $("#q9 #Next").click(function(){
         $(".questionForm").hide();
-        process("q9");
-        $("#q10").fadeIn(2000);
+        //process("q9");
+        $("#q10").fadeIn(200);
         return false;
     });
 
     $("#q10 #Next").click(function(){
         $(".questionForm").hide();
-        process("q10");
-        $("#q10").fadeIn(2000);
+        //process("q10");
+        $("#results").fadeIn(200);
         return false;
     });
+
+    $("#results")
 
 
     
